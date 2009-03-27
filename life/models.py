1b28dad652d8670af2cf465c4277345338982b2f
Zbigniew Braniecki (zbraniecki@mozilla.com) <Zbigniew Braniecki (zbraniecki@mozilla.com)>
2009-03-28 00:27:54 +0100

    initial commit of life app. It will keep models that maps on real life and is useful accross multiple apps... and will become an ultimate answer to life, universe and everything

---
+++ life/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Locale(models.Model):
+    """stores list of locales and their names
+    """
+    code = models.CharField(max_length = 30, unique = True)
+    name = models.CharField(max_length = 100, blank = True, null = True)
+
+    def __unicode__(self):
+        if self.name:
+            return '%s (%s)' % (self.name, self.code)
+        else:
+            return self.code
+
+class Repository(models.Model):
+    """stores repository list
+    """
+    name = models.CharField(max_length = 50)
+    url = models.URLField()
+
+    def __unicode__(self):
+        return self.name
+
+class Tree(models.Model):
+    """stores unique repositories combination like
+    comm-central + mozilla-central = Thunderbird trunk
+    releases/mozilla-1.9.1 = Firefox 3.5
+    mobile-browser + releases/mozilla-1.9.1 = Fennec 1.0
+    """
+    code = models.CharField(max_length = 50)
+    repositories = models.ManyToManyField(Repository)
+
+    def __unicode__(self):
+        return self.code
