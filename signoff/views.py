c3556e172f46bb17d0a42c0dbd70a67892bd9239
Zbigniew Braniecki (zbraniecki@mozilla.com) <Zbigniew Braniecki (zbraniecki@mozilla.com)>
2009-05-08 11:19:46 -0700

    clean up more code in signoff.views

--- signoff/views.py
+++ signoff/views.py
@@ -49,7 +49,7 @@ def locale_list(request, ms=None):
     if  mstone:
         while i < len(locales):
             locales[i].params = []
-            locales[i].params.append('Open' if _getstatus(locales[i], mstone)[0] else 'Unmatched dependencies')
+            locales[i].params.append('Open' if _getstatus(locales[i], mstone) else 'Unmatched dependencies')
             
             current = _get_current_signoff(locales[i], mstone)
             if current is not None:
@@ -83,7 +83,7 @@ def milestone_list(request, loc=None):
             mstones[i].params.append(str(mstones[i].start_event.date) + ' - ' + str(mstones[i].end_event.date))
 
         if locale:
-            mstones[i].params.append('Dependencies matches' if _getstatus(locale, mstones[i])[0] else 'Dependencies unmatched')
+            mstones[i].params.append('Dependencies matches' if _getstatus(locale, mstones[i]) else 'Dependencies unmatched')
             current = _get_current_signoff(locale, mstones[i])
             if current:
                 mstones[i].params.append('Signed off at %s by %s' % (current.when.strftime("%Y-%m-%d %H:%M"), current.author))
@@ -96,53 +96,14 @@ def milestone_list(request, loc=None):
     })
 
 def _getstatus(locale, mstone):
-    enabled = True
     today = datetime.date.today()
-
     if mstone.start_event.date < today and \
        mstone.end_event.date > today:
-        enabled = True
+        return True
     else:
-        enabled = False
-    return enabled
+        return False
 
-def signoff(request, loc, ms):
-    locale = Locale.objects.get(code=loc)
-    mstone = Milestone.objects.get(code=ms)
-    current = _get_current_signoff(locale, mstone)
-    enabled = _getstatus(locale, mstone)
-    user = request.user
-    anonymous = user.is_anonymous()
-    admin = user.is_staff
-
-    if request.method == 'POST' and enabled:
-        if anonymous:
-            request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s</span> could not be added - <strong>User not logged in</strong>' % (mstone, locale)
-        else:
-            if request.POST.has_key('accepted'):
-                form = AcceptForm(request.POST, instance=current)
-                if form.is_valid():
-                    form.save()
-                    if request.POST['accepted'] == "False":
-                        request.session['signoff_info'] = '<span style="font-style: italic">Declined'
-                    else:
-                        request.session['signoff_info'] = '<span style="font-style: italic">Accepted'
-                else:
-                    request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> could not be added' % (mstone, locale, user.username)
-            else:
-                instance = Signoff(milestone=mstone, locale=locale, author=user)
-                form = SignoffForm(request.POST, instance=instance)
-                if form.is_valid():
-                    form.save()
-                    request.session['signoff_info'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> added' % (mstone, locale, user.username)
-                else:
-                    request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> could not be added' % (mstone, locale, user.username)
-        return HttpResponseRedirect(reverse('signoff.views.sublist', kwargs={'arg':loc, 'arg2':ms}))
-
-    form = SignoffForm()
-    
-    forest = mstone.appver.tree.l10n
-    repo_url = '%s%s/' % (forest.url, locale.code)
+def _get_pushes(repo_url, current):
     pushobjs = Push.objects.filter(repository__url=repo_url).order_by('-push_date')[:10]
     
     pushes = []
@@ -176,40 +137,65 @@ def signoff(request, loc, ms):
                        'accepted': current.accepted if cur else None})
     if colspan > 0:
         pushes[len(pushes)-1-colspan]['colspan'] = colspan+1
+    return pushes
 
-    #request.session['signoff_info'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> added' % (mstone, locale, user.username)
-    #request.session['signoff_warning'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> warning' % (mstone, locale, user.username)
-    #request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> alert!' % (mstone, locale, user.username)
+def _get_notes(session):
     notes = {}
-    notes['info'] = request.session.get('signoff_info', None)
-    if notes['info']:
-        del request.session['signoff_info']
-    else:
-        del notes['info']
+    for i in ('info','warning','error'):
+        notes[i] = session.get('signoff_%s' % (i,), None)
+        if notes[i]:
+            del session['signoff_%s' % (i,)]
+        else:
+            del notes[i]
+    return notes
 
-    notes['warning'] = request.session.get('signoff_warning', None)
-    if notes['warning']:
-        del request.session['signoff_warning']
-    else:
-        del notes['warning']
+def signoff(request, loc, ms):
+    locale = Locale.objects.get(code=loc)
+    mstone = Milestone.objects.get(code=ms)
+    current = _get_current_signoff(locale, mstone)
+    enabled = _getstatus(locale, mstone)
+    user = request.user
+    anonymous = user.is_anonymous()
+    staff = user.is_staff
 
-    notes['error'] = request.session.get('signoff_error', None)
-    if notes['error']:
-        del request.session['signoff_error']
-    else:
-        del notes['error']
+    if request.method == 'POST' and enabled: # we're going to process forms
+        if anonymous: # ... but we're not logged in. Panic!
+            request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s</span> could not be added - <strong>User not logged in</strong>' % (mstone, locale)
+        else:
+            if request.POST.has_key('accepted'): # we're in AcceptedForm mode
+                if not staff: # ... but we have no privileges for that!
+                    request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s</span> could not be accepted/rejected - <strong>User has not enough privileges</strong>' % (mstone, locale)
+                else:
+                    form = AcceptForm(request.POST, instance=current)
+                    if form.is_valid():
+                        form.save()
+                        if request.POST['accepted'] == "False":
+                            request.session['signoff_info'] = '<span style="font-style: italic">Rejected'
+                        else:
+                            request.session['signoff_info'] = '<span style="font-style: italic">Accepted'
+                    else:
+                        request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> could not be added' % (mstone, locale, user.username)
+            else:
+                instance = Signoff(milestone=mstone, locale=locale, author=user)
+                form = SignoffForm(request.POST, instance=instance)
+                if form.is_valid():
+                    form.save()
+                    request.session['signoff_info'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> added' % (mstone, locale, user.username)
+                else:
+                    request.session['signoff_error'] = '<span style="font-style: italic">Signoff for %s %s by %s</span> could not be added' % (mstone, locale, user.username)
+        return HttpResponseRedirect(reverse('signoff.views.sublist', kwargs={'arg':loc, 'arg2':ms}))
+
+    form = SignoffForm()
     
-    if not current:
-        curcol = 0
-    elif current.accepted == True:
-        curcol = 1
-    elif current.accepted == False:
-        curcol = -1
-    elif current.accepted == None:
-        curcol = 0
-    
-    accepted = Signoff.objects.filter(locale=locale, milestone=mstone, accepted=True).order_by('-pk')
-    accepted =  accepted[0] if accepted else None
+    forest = mstone.appver.tree.l10n
+    repo_url = '%s%s/' % (forest.url, locale.code)
+    pushes = _get_pushes(repo_url, current)
+    notes = _get_notes(request.session)
+    curcol = {None:0,False:-1,True:1}[current.accepted] if current else 0
+    try:
+        accepted = Signoff.objects.filter(locale=locale, milestone=mstone, accepted=True).order_by('-pk')[0].get()
+    except:
+        accepted = None
     return render_to_response('signoff/signoff.html', {
         'mstone': mstone,
         'locale': locale,
