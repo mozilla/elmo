f453d7c5f56b5cebd6f456d027b65b4ea052f097
Zbigniew Braniecki (zbraniecki@mozilla.com) <Zbigniew Braniecki (zbraniecki@mozilla.com)>
2009-06-16 03:06:41 +0200

    move uri scheme to dashboard

--- signoff/urls.py
+++ signoff/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('signoff.views',
-    (r'^\/?$', 'index'),
+    (r'^\/?$', 'milestone_list'),
     (r'^\/api\/pushes$', 'get_api_items'),
     (r'^\/milestones\/([^/]+)\/l10n-changesets$', 'l10n_changesets'),
     (r'^\/milestones\/([^/]+)\/shipped-locales$', 'shipped_locales'),
     (r'^\/locales\/?$', 'locale_list'),
     (r'^\/locales\/(?P<loc>[^\/]+)', 'milestone_list'),
-    (r'^\/milestones\/?$', 'milestone_list'),
-    (r'^\/milestones\/(?P<ms>[^/]+)$', 'locale_list'),
+    (r'^\/milestones\/?$', 'dashboard'),
+    (r'^\/milestones\/(?P<ms>[^/]+)$', 'dashboard'),
     (r'^\/milestones\/([^/]+)/dashboard$', 'dashboard'),
     (r'^\/milestones\/([^/]+)/json$', 'json'),
     (r'^\/(?P<arg>[^\/]+)?\/(?P<arg2>[^\/]+)?$', 'sublist'),
