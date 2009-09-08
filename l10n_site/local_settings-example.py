0fb779d072c62229d3314a240fa604090f02cce8
Axel Hecht <l10n@mozilla.com>
2009-09-08 21:00:06 +0200

    factor out the db stuff into a separate file, hgignore that

---
+++ l10n_site/local_settings-example.py
@@ -0,0 +1,11 @@
+# copy this file to local_setttings.py and enter your local settings in.
+# This file is included by settings.py by both django-site and
+# l10n-master to connect to the db etc.
+
+DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+REPOSITORY_BASE = ''
--- l10n_site/settings.py
+++ l10n_site/settings.py
@@ -1,5 +1,5 @@
 # Django settings for dashboard project.
-
+from local_settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,13 +9,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '../db_dash'   # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -100,6 +93,3 @@ INSTALLED_APPS = (
     'tinder',
 )
 
-# dashboard's own settings
-
-REPOSITORY_BASE = '/Users/axelhecht/src/central/'
