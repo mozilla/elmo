81be1cc876fe172b9d55bad6c1e20fda59148ac3
Zbigniew Braniecki <gandalf@mozilla.com>
2009-10-22 01:47:26 +0200

    exclude changesets from repository adding form in admin panel

--- life/admin.py
+++ life/admin.py
@@ -1,7 +1,10 @@
 from life.models import Tree, Repository, Locale, Forest
 from django.contrib import admin
 
+class RepositoryAdmin(admin.ModelAdmin):
+    exclude = ('changesets',)
+
 admin.site.register(Locale)
-admin.site.register(Repository)
+admin.site.register(Repository, RepositoryAdmin)
 admin.site.register(Tree)
 admin.site.register(Forest)
