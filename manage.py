1e178b9963e97c5f9c1be392b5e4502ecf00c349
Peter Bengtsson <mail@peterbe.com>
2011-04-27 20:02:48 +0200

    correct location of safe_django_forms import

--- manage.py
+++ manage.py
@@ -37,9 +37,9 @@ import settings
 import djcelery
 djcelery.setup_loader()
 
-# Monkey-patch django forms to avoid having to use Jinja2's |safe everywhere.
-import safe_django_forms
-safe_django_forms.monkeypatch()
-
 if __name__ == "__main__":
     execute_manager(settings)
+
+    # Monkey-patch django forms to avoid having to use Jinja2's |safe everywhere.
+    import safe_django_forms
+    safe_django_forms.monkeypatch()
