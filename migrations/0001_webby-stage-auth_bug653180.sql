2e9090295ef48806c4fc263d0162cdce88381aa4
Gervase Markham <gerv@gerv.net>
2012-04-25 13:07:09 +0100

    bug 715600, Update license to MPL 2, r=pike

--- migrations/0001_webby-stage-auth_bug653180.sql
+++ migrations/0001_webby-stage-auth_bug653180.sql
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 ALTER TABLE `webby_project` ADD `stage_login` varchar(80);
 ALTER TABLE `webby_project` ADD `stage_passwd` varchar(80);
 ALTER TABLE `webby_project` ADD `stage_auth_url` varchar(250);
