1263a8836b61326a320c3da0e2be12b2339700b5
Gervase Markham <gerv@gerv.net>
2012-04-25 13:07:09 +0100

    Update license to MPL 2.

--- migrations/0002_webby-archived-flag_bug640155.sql
+++ migrations/0002_webby-archived-flag_bug640155.sql
@@ -1,1 +1,5 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 ALTER TABLE `webby_project` ADD COLUMN `is_archived` bool NOT NULL DEFAULT FALSE;
