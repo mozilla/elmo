a3d2c186d85fd4cafaf75e26d2205bf2f3c21f21
Peter Bengtsson <mail@peterbe.com>
2012-05-10 10:53:06 -0700

    adding back in ROOT_PACKAGE as installed app to fix broken collectstatic, r=bustage

--- settings/base.py
+++ settings/base.py
@@ -10,6 +10,9 @@ from django.utils.functional import lazy
 
 DEBUG = TEMPLATE_DEBUG = False
 
+# because we're not using the pythonic structure as per funfactory
+ROOT_PACKAGE = os.path.basename(ROOT)
+
 ADMINS = ()
 MANAGERS = ADMINS
 
@@ -94,6 +97,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 INSTALLED_APPS += (
+    ROOT_PACKAGE,
+
     # Local apps
     'commons',
     'nashvegas',
