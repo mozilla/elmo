528920ffaf05acc109d05b42c2b22d9eb6c3476b
Peter Bengtsson <mail@peterbe.com>
2012-06-11 16:25:12 -0700

    bug672545 - class based view decorator utility, r=Pike

---
+++ apps/shipping/views/utils.py
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Utilities for helping the shipping views"""
+
+from django.utils.decorators import method_decorator
+
+
+def class_decorator(decorator):
+    def inner(cls):
+        orig_dispatch = cls.dispatch
+
+        @method_decorator(decorator)
+        def new_dispatch(self, request, *args, **kwargs):
+            return orig_dispatch(self, request, *args, **kwargs)
+        cls.dispatch = new_dispatch
+        return cls
+
+    return inner
