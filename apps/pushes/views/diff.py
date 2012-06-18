8b336057cee05d38e3137b5ad830a7d705a21aef
Axel Hecht <axel@pike.org>
2012-06-18 13:04:01 +0200

    bug 655942, factor some worker code of the diff view

--- apps/pushes/views/diff.py
+++ apps/pushes/views/diff.py
@@ -26,6 +26,10 @@ from Mozilla.Parser import getParser
 from Mozilla.CompareLocales import AddRemove, Tree as DataTree
 
 
+class BadRevision(Exception):
+    "Revision could not be resolved"
+    pass
+
 
 class DiffView(View):
 
@@ -37,7 +41,6 @@ class DiffView(View):
         if not request.GET.get('repo'):
             return http.HttpResponseBadRequest("Missing 'repo' parameter")
         reponame = request.GET['repo']
-        repopath = settings.REPOSITORY_BASE + '/' + reponame
         try:
             repo_url = Repository.objects.get(name=reponame).url
         except Repository.DoesNotExist:
@@ -46,146 +49,32 @@ class DiffView(View):
             return http.HttpResponseBadRequest("Missing 'from' parameter")
         if not request.GET.get('to'):
             return http.HttpResponseBadRequest("Missing 'to' parameter")
-
-        ui = _ui()
-        repo = repository(ui, repopath)
-        # Convert the 'from' and 'to' to strings (instead of unicode)
-        # in case mercurial needs to look for the key in binary data.
-        # This prevents UnicodeWarning messages.
         try:
-            ctx1 = repo.changectx(str(request.GET['from']))
-        except RepoLookupError:
-            return http.HttpResponseBadRequest("Unrecognized 'from' parameter")
-        try:
-            ctx2 = repo.changectx(str(request.GET['to']))
-        except RepoLookupError:
-            return http.HttpResponseBadRequest("Unrecognized 'to' parameter")
-        copies = pathcopies(ctx1, ctx2)
-        match = None  # maybe get something from l10n.ini and cmdutil
-        changed, added, removed = repo.status(ctx1, ctx2, match=match)[:3]
-
-        # split up the copies info into thos that were renames and those that
-        # were copied.
-        moved = {}
-        copied = {}
-        for new_name, old_name in copies.items():
-            if old_name in removed:
-                moved[new_name] = old_name
-            else:
-                copied[new_name] = old_name
-
-        paths = ([(f, 'changed') for f in changed]
-                 + [(f, 'removed') for f in removed
-                    if f not in moved.values()]
-                 + [(f,
-                     (f in moved and 'moved') or
-                     (f in copied and 'copied')
-                     or 'added') for f in added])
+            paths = self.contextsAndPaths(request.GET['from'],
+                                          request.GET['to'],
+                                          reponame)
+        except BadRevision, e:
+            return http.HttpResponseBadRequest(e.args[0])
         diffs = DataTree(dict)
         for path, action in paths:
-            lines = []
-            try:
-                p = getParser(path)
-            except UserWarning:
-                diffs[path].update({
-                  'path': path,
-                  'isFile': True,
-                  'rev': ((action == 'removed') and request.GET['from']
-                         or request.GET['to']),
-                  'class': action,
-                  'renamed': moved.get(path),
-                  'copied': copied.get(path)
+            lines = self.diffLines(path, action)
+            v = {'path': path,
+                 'renamed': self.moved.get(path),
+                 'copied': self.copied.get(path)}
+            if lines is None:
+                v.update({
+                    'isFile': True,
+                    'class': action,
+                    'rev': ((action == 'removed') and request.GET['from']
+                            or request.GET['to'])
                 })
-                continue
-            if action == 'added':
-                a_entities = []
-                a_map = {}
             else:
-                realpath = (action == 'moved' and moved[path] or
-                            action == 'copied' and copied[path] or
-                            path)
-                data = ctx1.filectx(realpath).data()
-                data = self._universal_newlines(data)
-                try:
-                    p.readContents(data)
-                    a_entities, a_map = p.parse()
-                except:
-                    # consider doing something like:
-                    # logging.warn('Unable to parse %s', path, exc_info=True)
-                    diffs[path].update({
-                      'path': path,
-                      'isFile': True,
-                      'rev': ((action == 'removed') and request.GET['from']
-                              or request.GET['to']),
-                      'class': action,
-                      'renamed': moved.get(path),
-                      'copied': copied.get(path)
-                    })
-                    continue
-
-            if action == 'removed':
-                c_entities, c_map = [], {}
-            else:
-                data = ctx2.filectx(path).data()
-                data = self._universal_newlines(data)
-                try:
-                    p.readContents(data)
-                    c_entities, c_map = p.parse()
-                except:
-                    # consider doing something like:
-                    # logging.warn('Unable to parse %s', path, exc_info=True)
-                    diffs[path].update({
-                      'path': path,
-                      'isFile': True,
-                      'rev': ((action == 'removed') and request.GET['from']
-                             or request.GET['to']),
-                      'class': action
-                    })
-                    continue
-            a_list = sorted(a_map.keys())
-            c_list = sorted(c_map.keys())
-            ar = AddRemove()
-            ar.set_left(a_list)
-            ar.set_right(c_list)
-            for action, item_or_pair in ar:
-                if action == 'delete':
-                    lines.append({
-                      'class': 'removed',
-                      'oldval': [{'value':a_entities[a_map[item_or_pair]].val}],
-                      'newval': '',
-                      'entity': item_or_pair
-                    })
-                elif action == 'add':
-                    lines.append({
-                      'class': 'added',
-                      'oldval': '',
-                      'newval': [{'value': c_entities[c_map[item_or_pair]].val}],
-                      'entity': item_or_pair
-                    })
-                else:
-                    oldval = a_entities[a_map[item_or_pair[0]]].val
-                    newval = c_entities[c_map[item_or_pair[1]]].val
-                    if oldval == newval:
-                        continue
-                    sm = SequenceMatcher(None, oldval, newval)
-                    oldhtml = []
-                    newhtml = []
-                    for op, o1, o2, n1, n2 in sm.get_opcodes():
-                        if o1 != o2:
-                            oldhtml.append({'class': op, 'value': oldval[o1:o2]})
-                        if n1 != n2:
-                            newhtml.append({'class': op, 'value': newval[n1:n2]})
-                    lines.append({'class': 'changed',
-                                  'oldval': oldhtml,
-                                  'newval': newhtml,
-                                  'entity': item_or_pair[0]})
-            container_class = lines and 'file' or 'empty-diff'
-            diffs[path].update({'path': path,
-                                'class': container_class,
-                                'lines': lines,
-                                'renamed': moved.get(path),
-                                'copied': copied.get(path)
-                                })
+                container_class = lines and 'file' or 'empty-diff'
+                v.update({
+                    'class': container_class,
+                    'lines': lines
+                })
+            diffs[path].update(v)
         diffs = diffs.toJSON().get('children', [])
         return render(request, 'pushes/diff.html', {
                         'given_title': request.GET.get('title', None),
@@ -195,6 +84,119 @@ class DiffView(View):
                         'new_rev': request.GET['to'],
                         'diffs': diffs
                       })
+
+    def contextsAndPaths(self, _from, _to, suggested_repo):
+        repopath = settings.REPOSITORY_BASE + '/' + suggested_repo
+        ui = _ui()
+        repo = repository(ui, repopath)
+        # Convert the 'from' and 'to' to strings (instead of unicode)
+        # in case mercurial needs to look for the key in binary data.
+        # This prevents UnicodeWarning messages.
+        try:
+            self.ctx1 = repo.changectx(str(_from))
+        except RepoLookupError:
+            raise BadRevision("Unrecognized 'from' parameter")
+        try:
+            self.ctx2 = repo.changectx(str(_to))
+        except RepoLookupError:
+            raise BadRevision("Unrecognized 'to' parameter")
+        copies = pathcopies(self.ctx1, self.ctx2)
+        match = None  # maybe get something from l10n.ini and cmdutil
+        changed, added, removed = repo.status(self.ctx1, self.ctx2,
+                                              match=match)[:3]
+
+        # split up the copies info into thos that were renames and those that
+        # were copied.
+        self.moved = {}
+        self.copied = {}
+        for new_name, old_name in copies.items():
+            if old_name in removed:
+                self.moved[new_name] = old_name
+            else:
+                self.copied[new_name] = old_name
+
+        paths = ([(f, 'changed') for f in changed]
+                 + [(f, 'removed') for f in removed
+                    if f not in self.moved.values()]
+                 + [(f,
+                     (f in self.moved and 'moved') or
+                     (f in self.copied and 'copied')
+                     or 'added') for f in added])
+        return paths
+
+    def diffLines(self, path, action):
+        lines = []
+        try:
+            p = getParser(path)
+        except UserWarning:
+            return None
+        if action == 'added':
+            a_entities = []
+            a_map = {}
+        else:
+            realpath = (action == 'moved' and self.moved[path] or
+                        action == 'copied' and self.copied[path] or
+                        path)
+            data = self.ctx1.filectx(realpath).data()
+            data = self._universal_newlines(data)
+            try:
+                p.readContents(data)
+                a_entities, a_map = p.parse()
+            except:
+                # consider doing something like:
+                # logging.warn('Unable to parse %s', path, exc_info=True)
+                return None
+
+        if action == 'removed':
+            c_entities, c_map = [], {}
+        else:
+            data = self.ctx2.filectx(path).data()
+            data = self._universal_newlines(data)
+            try:
+                p.readContents(data)
+                c_entities, c_map = p.parse()
+            except:
+                # consider doing something like:
+                # logging.warn('Unable to parse %s', path, exc_info=True)
+                return None
+        a_list = sorted(a_map.keys())
+        c_list = sorted(c_map.keys())
+        ar = AddRemove()
+        ar.set_left(a_list)
+        ar.set_right(c_list)
+        for action, item_or_pair in ar:
+            if action == 'delete':
+                lines.append({
+                  'class': 'removed',
+                  'oldval': [{'value':a_entities[a_map[item_or_pair]].val}],
+                  'newval': '',
+                  'entity': item_or_pair
+                })
+            elif action == 'add':
+                lines.append({
+                  'class': 'added',
+                  'oldval': '',
+                  'newval': [{'value': c_entities[c_map[item_or_pair]].val}],
+                  'entity': item_or_pair
+                })
+            else:
+                oldval = a_entities[a_map[item_or_pair[0]]].val
+                newval = c_entities[c_map[item_or_pair[1]]].val
+                if oldval == newval:
+                    continue
+                sm = SequenceMatcher(None, oldval, newval)
+                oldhtml = []
+                newhtml = []
+                for op, o1, o2, n1, n2 in sm.get_opcodes():
+                    if o1 != o2:
+                        oldhtml.append({'class': op, 'value': oldval[o1:o2]})
+                    if n1 != n2:
+                        newhtml.append({'class': op, 'value': newval[n1:n2]})
+                lines.append({'class': 'changed',
+                              'oldval': oldhtml,
+                              'newval': newhtml,
+                              'entity': item_or_pair[0]})
+        return lines
 
 
 diff = DiffView.as_view()
