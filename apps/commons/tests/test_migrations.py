8c9093396550eda3f20dda362470c2b7381f3f23
Adrian Gaudebert <adrian@gaudebert.fr>
2013-07-28 00:01:28 +0200

    bug 889015, fixed the unit tests for South migrations, r=peterbe

--- apps/commons/tests/test_migrations.py
+++ apps/commons/tests/test_migrations.py
@@ -10,15 +10,30 @@ class MigrationTests(test_utils.TestCase):
     """Sanity checks for the SQL migration scripts."""
 
     @staticmethod
-    def _migrations_path():
+    def _migrations_path(app_path):
         """Return the absolute path to the migration script folder."""
-        return os.path.join(settings.ROOT, 'migrations')
+        return os.path.join(app_path, 'migrations')
 
-    def test_unique(self):
+    @staticmethod
+    def _apps_path():
+        """Return the absolute path to the apps folder."""
+        return os.path.join(settings.ROOT, 'apps')
+
+    def test_uniques(self):
         """Assert that the numeric prefixes of the DB migrations are unique."""
+        # list all apps
+        # for each app, check if there's a migrations folder
+        # then test for uniqueness in that folder
+        path = self._apps_path()
+        for app in os.listdir(path):
+            app_path = os.path.join(path, app)
+            migrations_path = self._migrations_path(app_path)
+            if os.path.exists(migrations_path):
+                self._test_unique(migrations_path)
+
+    def _test_unique(self, path):
         leading_digits = re.compile(r'^\d+')
         seen_numbers = set()
-        path = self._migrations_path()
         for filename in os.listdir(path):
             __, ext = os.path.splitext(filename)
             # prevent matching backup files (e.g. 001_foo.sql~)
