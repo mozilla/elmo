6a225b1e6550ac68b8af1488ed92f086161726f6
Axel Hecht <axel@pike.org>
2017-04-06 13:22:47 +0200

    Django Deprecations: start using SessionAuthenticationMiddleware
    Not really impactful on elmo, as we don't have passwords in
    elmo itself, and thus never change them in the django setting.

--- elmo/settings/base.py
+++ elmo/settings/base.py
@@ -88,6 +88,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'session_csrf.CsrfMiddleware',
 
