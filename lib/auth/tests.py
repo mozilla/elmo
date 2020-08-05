c21438ee462c2f672598331c27c7eae500db08e9
Axel Hecht <axel@pike.org>
2020-08-05 14:41:52 +0200

    Update requirements (#67)
    * Update prod requirements
    * Update dev requirements
    * Use builtin unittest.mock on py3

--- lib/auth/tests.py
+++ lib/auth/tests.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from mock import patch
+from unittest.mock import patch
 
 from elmo.test import TestCase
 from django.contrib.auth.models import Group
